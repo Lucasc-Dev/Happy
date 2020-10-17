3dea3c4293625525ba971dfb8442e0fbd479ebb1
Lucas César <lucasambrogi1@hotmail.com>
2020-10-16 23:32:22 -0300

    Mapbox implemented on OrphanagesMap page.

--- frontend/src/pages/OrphanagesMap/index.tsx
+++ frontend/src/pages/OrphanagesMap/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarketImg from '../../assets/icons/MapMarker.svg';
 
 import { 
   Container,
   SideBar,
-  Map,
   LinkButton,
 } from './styles';
 
@@ -27,9 +29,17 @@ const OrphanagesMap: React.FC = () => {
         </footer>
       </SideBar>
 
-      <Map></Map>
+      <Map 
+        center={[-23.3546962, -47.8518381]}
+        zoom={15}
+        style={{ width: '100%', height: '100%', zIndex: 5 }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+        />
+      </Map>
 
-      <LinkButton to="a" >
+      <LinkButton to="" >
         <FiPlus size={32} color="#FFF" />
       </LinkButton>
     </Container>
